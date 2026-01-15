29a728472518f956685062029d20786df0bcbaf3
Lucas Vocanson <45082657+lvocanson@users.noreply.github.com>
2026-01-15 15:12:33 +0100

    Items transformation & recipes skeleton

---
+++ Source/Interactions/Public/Interactions/InteractionTest.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractionBase.h"
+#include "InteractionTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class INTERACTIONS_API UInteractionTest : public UInteractionBase
+{
+	GENERATED_BODY()
+	
+	void StartInteraction(const FInteractionContext& Context) override;
+};
---
+++ Source/Recipes/Public/RecipeSystem.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "RecipeSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RECIPES_API URecipeSystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+};
