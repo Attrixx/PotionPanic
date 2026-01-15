cadba217cc6a8dbdff713c2121862cdbccbe3d09
Lucas Vocanson <45082657+lvocanson@users.noreply.github.com>
2026-01-15 15:12:33 +0100

    Items transformation & recipes skeleton

---
+++ Source/Recipes/Public/RecipeAsset.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "RecipeAsset.generated.h"
+
+class UItemTransformation;
+
+/**
+ * 
+ */
+UCLASS()
+class RECIPES_API URecipeAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText RecipeName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<TObjectPtr<UItemTransformation>> Steps;
+};
