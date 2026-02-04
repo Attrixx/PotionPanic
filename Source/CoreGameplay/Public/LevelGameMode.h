880d6009cab43842afd20844f8ae249357b715b9
Lucas Vocanson <45082657+lvocanson@users.noreply.github.com>
2026-02-04 11:07:42 +0100

    Camera: Set view target on player login

---
+++ Source/CoreGameplay/Public/LevelGameMode.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LevelGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COREGAMEPLAY_API ALevelGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+protected:
+	
+	void OnPostLogin(AController* NewPlayer) override;
+	
+protected:
+	
+	UPROPERTY(EditAnywhere)
+	FName ViewTargetTag = FName("ViewTarget");
+	
+private:
+	
+	AActor* GetViewTarget();
+	TWeakObjectPtr<AActor> CachedViewTarget;
+};
