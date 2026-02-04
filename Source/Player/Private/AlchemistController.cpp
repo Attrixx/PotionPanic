31619cf7c6d81e3c3a8445e47492c5b5c36df099
Lucas Vocanson <45082657+lvocanson@users.noreply.github.com>
2026-02-04 11:07:42 +0100

    Camera: Set view target on player login

---
+++ Source/Player/Private/AlchemistController.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AlchemistController.h"
+
+AAlchemistController::AAlchemistController()
+{
+	bAutoManageActiveCameraTarget = false;
+}
